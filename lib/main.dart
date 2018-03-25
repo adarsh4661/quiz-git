28b16c85576f76612734fabb170d50d5343e1fd8
aadarsh <adarsh4661@live.com>
2018-03-25 22:34:53 +0530

    23

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import './pages/landing_page.dart';
 import './pages/quiz_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main(){
    runApp(new MaterialApp(
-     home: new QuizPage() ,
+     title: 'Google Sign In',
+      home: new SignInDemo(),
    ));
 }
